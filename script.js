6908c57c695d5c22b243ae3f8c0504b63b8982c0
Kevin-CodeCrew <kevin@code-crew.org>
2018-08-13 23:27:33 -0500

    Graded 08-13-2018 23:27:24

---
+++ script.js
@@ -0,0 +1,18 @@
+var people = [
+    {
+        firstName: "Jimmy",
+        lastName: "Page",
+        age: 62,
+        zipCode: "00821"
+
+    },
+    {
+        firstName: "Jimmy",
+        lastName: "Page",
+        age: 62,
+        zipCode: "00821"
+
+    }
+]
+
+console.Write(people);
